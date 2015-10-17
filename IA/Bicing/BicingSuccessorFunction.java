3a4cf4586b24a3da59353e1f91eb27ee33a5c05f
Marah <carlos.maria_rodriguez@hotmail.com>
2015-10-17 19:25:42 +0200

    Quitar comm

--- IA/Bicing/BicingSuccessorFunction.java
+++ IA/Bicing/BicingSuccessorFunction.java
@@ -9,9 +9,7 @@ import aima.search.framework.SuccessorFunction;
 public class BicingSuccessorFunction implements SuccessorFunction{
 
   public List getSuccessors(Object state) {
-//     System.out.println("successors");
     BicingState situation = (BicingState) state;
-//     System.out.println(situation.cars);
     List successors = new ArrayList();
     if(situation.cars == 0) return successors;
     for(int i = 0; i < situation.Sd.size(); ++i){
@@ -23,22 +21,17 @@ public class BicingSuccessorFunction implements SuccessorFunction{
 	    for(int bicis = 1; bicis <= Math.min(30,Math.min(s1.Available(),-s2.Difer())); ++bicis){
 	      Trip oper = new Trip(i,bicis,j);
 	      BicingFirstHeuristicFunction h1 = new BicingFirstHeuristicFunction();
-// 	      System.out.println(h1.getHeuristicValue(situation));
 	      BicingState newState = new BicingState(situation,oper);
-// 	      System.out.println(oper.identifier());
-// 	      System.out.println(h1.getHeuristicValue(situation));
 	      successors.add(new Successor(oper.identifier(),newState));
 	    }
 	    for(int k = 0; k < situation.Sd.size(); ++k){
 	      if(k == j) continue;
 	      StationData s3 = situation.Sd.get(k);
 	      if(s3.Difer() < 0){
-// 		System.out.println(s1.Available()+" "+s2.Difer()+" "+s3.Difer());
 		for(int bicis = 1; bicis <= Math.min(30,Math.min(s1.Available(),-s2.Difer()-s3.Difer())); ++bicis){
 		  for(int bicis2 = 1; bicis2 < Math.min(bicis,-s2.Difer()+1); ++bicis2){
 		    Trip oper = new Trip(i,bicis2,j,bicis-bicis2,k);
 		    BicingState newState = new BicingState(situation,oper);
-// 		    System.out.println(oper.identifier());
 		    successors.add(new Successor(oper.identifier(),newState));
 		  }
 		}
@@ -51,7 +44,6 @@ public class BicingSuccessorFunction implements SuccessorFunction{
 //     Trip oper = new Trip(-1,0,0);
 //     BicingState newState = new BicingState(situation,oper);
 //     successors.add(new Successor(oper.identifier(),newState));
-//     System.out.println(successors.size());
     return successors;
   }
 };
